89b8b8b7cc721c9b9ba86309a4a382a73ee30611
seungjun-green <lsj3285007@gmail.com>
2024-08-24 03:50:37 +0900

    added BCE loss

--- numpy_pytorch/loss.py
+++ numpy_pytorch/loss.py
@@ -8,3 +8,12 @@ class MeanSquaredError:
     @staticmethod
     def loss_derivative(predicted, target):
         return 2 * (predicted - target) / target.size
+    
+class BinaryCrossEntropy:
+    @staticmethod
+    def loss(predicted, target):
+        return -np.mean(target * np.log(predicted) + (1 - target) * np.log(1 - predicted))
+
+    @staticmethod
+    def loss_derivative(predicted, target):
+        return (predicted - target) / (predicted * (1 - predicted))
